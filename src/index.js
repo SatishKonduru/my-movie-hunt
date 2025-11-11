c3fb6b0dd79c6d82a4e4cdad0b0997e4f2f1e76c
satish <satishkonduru0225@gmail.com>
2025-11-11 06:50:59 +0530

    Star Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from './App';
+import StarRating from "./StarRating";
 
-
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="red" maxRating={10} onSetRating={setMovieRating} />
+      <h2>This Movie was rated {movieRating} stars</h2>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <StarRating maxRating={10} />
+    <StarRating size={24} />
+    <StarRating size={60} color="#f00" />
+    <StarRating size={60} color="snow" />
+    <StarRating size={60} color="tomato" /> */}
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={4}
+    />
+    <StarRating maxRating={10} color="tomato" size={60} /> */}
+    <Test />
   </React.StrictMode>
 );
-
